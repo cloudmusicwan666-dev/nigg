762e127c5a68cd9d2294520ec9503952858bbe2c
cloudmusicwan666-dev <cloudmusicwan666@gmail.com>
2025-10-18 14:30:26 +0800

    Create products.php

---
+++ includes/data/products.php
@@ -0,0 +1,49 @@
+<?php
+return [
+    'starterKits' => [
+        [
+            'name' => 'RELX Phantom 入门套装',
+            'tagline' => '轻盈机身，智能气压感应',
+            'flavors' => ['经典烟草', '冰镇西柚', '绿豆冰'],
+            'price' => '¥328',
+            'image' => 'images/relx-phantom-kit.jpg'
+        ],
+        [
+            'name' => 'RELX Infinity 套装',
+            'tagline' => '磁吸充电，全天候续航',
+            'flavors' => ['冰镇青提', '冷泡铁观音'],
+            'price' => '¥368',
+            'image' => 'images/relx-infinity-kit.jpg'
+        ]
+    ],
+    'pods' => [
+        [
+            'name' => 'RELX 灵点烟弹四代',
+            'tagline' => '升级防漏结构，口感更顺滑',
+            'flavors' => ['桂花乌龙', '冷萃拿铁', '青芒爆珠'],
+            'price' => '¥99',
+            'image' => 'images/relx-pod-4.jpg'
+        ],
+        [
+            'name' => 'RELX 五代 Pods',
+            'tagline' => '五层防漏设计，持久还原',
+            'flavors' => ['初雪山茶', '青柠苏打'],
+            'price' => '¥118',
+            'image' => 'images/relx-pod-5.jpg'
+        ]
+    ],
+    'accessories' => [
+        [
+            'name' => 'RELX 旅行便携包',
+            'tagline' => '防震防尘，出行必备',
+            'price' => '¥68',
+            'image' => 'images/relx-travel-case.jpg'
+        ],
+        [
+            'name' => '磁吸充电底座',
+            'tagline' => '即放即充，桌面美学',
+            'price' => '¥128',
+            'image' => 'images/relx-dock.jpg'
+        ]
+    ]
+];
